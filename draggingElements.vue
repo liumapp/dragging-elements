caf20d4dc9c2ab2f2bf4b637c900ec705256a749
tanghuiming <thm133@foxmail.com>
2018-11-01 20:14:30 +0800

    touch

--- draggingElements.vue
+++ draggingElements.vue
@@ -6,7 +6,7 @@
  * @date 2018/10/16
  */
 <template>
-  <div class="vdr" :class="{ draggable: draggable && !disable, resizable: resizable && !disable, active , dragging, resizing}" @mousedown.stop="elmDown" tabindex="0" :style="style">
+  <div class="vdr"  @touchstart='touchStart' @touchmove='touchMove' @touchend='touchEnd' :class="{ draggable: draggable && !disable, resizable: resizable && !disable, active , dragging, resizing}" @mousedown.stop="elmDown" tabindex="0" :style="style">
     <!-- 如果可改变大小为真 -->
     <template v-if="resizable && !disable">
       <!-- 待优化 -->
@@ -144,7 +144,15 @@
         elmX: 0,
         elmY: 0,
         elmW: 0,
-        elmH: 0
+        elmH: 0,
+        startX:0,//开始触摸的位置
+        startY:0,
+        moveX:0,//滑动时的位置
+        moveY:0,
+        endX:0,//结束触摸的位置
+        endY:0,
+        disX:0,//移动距离
+        disY:0
       }
     },
     mounted () {
@@ -164,6 +172,75 @@
       this.$emit('resizing', this.left, this.top, this.width, this.height)
     },
     methods: {
+      touchStart(ev) {
+          ev = ev || event;
+          ev.preventDefault();
+          if(ev.touches.length == 1) {    //tounches类数组，等于1时表示此时有只有一只手指在触摸屏幕
+              this.startX = ev.touches[0].clientX; // 记录开始位置
+              this.startY = ev.touches[0].clientY; // 记录开始位置
+              console.log("start===="+this.startX,this.startY)
+          }
+      },
+      touchMove:function(ev) {
+          ev = ev || event;
+          ev.preventDefault();
+          console.log("move")
+          if(ev.touches.length == 1) {
+              //滑动时距离浏览器左侧的距离
+              this.moveX = ev.touches[0].clientX;
+              this.moveY = ev.touches[0].clientY;
+              console.log(this.moveX,this.moveY)
+              //实时的滑动的距离-起始位置=实时移动的位置
+              this.disX = this.moveX-this.startX;
+              this.disY = this.moveY-this.startY;
+              this.top += this.disY; //move过程中实时改变top及left值
+              this.left += this.disX;
+              this.startX += this.disX; //每一次位置改变后touchmove的开始操作位置也需要改变
+              this.startY += this.disY;
+
+              //以下对其边缘做判断，拖拽块不会跑出父元素区域
+              if(this.top<0){
+                this.top = 0
+              }
+              if(this.left<0){
+                this.left = 0;
+              }
+              if(this.left+this.w>this.parentW){
+                this.left = this.parentW - this.w;
+              }
+              if(this.top+this.h>this.parentH){
+                this.top = this.parentH - this.h;
+              }
+          }
+      },
+      touchEnd:function(ev){
+          ev = ev || event;
+          ev.preventDefault();
+          console.log("end")
+          if(ev.changedTouches.length == 1) {
+              let endX = ev.changedTouches[0].clientX;
+              let endY = ev.changedTouches[0].clientY;
+              this.disX = endX-this.startX;
+              this.disY = endY-this.startY;
+              console.log("偏移了==="+this.disX,this.disY)
+              this.top += this.disY;
+              this.left += this.disX;
+
+              //以下对其边缘做判断，拖拽块不会跑出父元素区域
+              if(this.top<0){
+                this.top = 0
+              }
+              if(this.left<0){
+                this.left = 0;
+              }
+              if(this.left+this.w>this.parentW){
+                this.left = this.parentW - this.w;
+              }
+              if(this.top+this.h>this.parentH){
+                this.top = this.parentH - this.h;
+              }
+          }
+      },
       calculationParent () {
         this.parentW = parseInt(this.$el.parentNode.clientWidth, 10)
         this.parentH = parseInt(this.$el.parentNode.clientHeight, 10)
